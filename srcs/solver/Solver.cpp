177188bd1e98dd6ebf9ed9898c97802e2a5659f5
ImperatorS79 <fevrier.simon@gmail.com>
2020-03-13 00:20:30 +0100

    Fix modification due to merge conflict

--- srcs/solver/Solver.cpp
+++ srcs/solver/Solver.cpp
@@ -85,7 +85,9 @@ Solver::~Solver()
 
 void Solver::applyBoundaryConditions()
 {
-	  assert(!m_mesh.nodesList.empty());
+    assert(m_A.rows() == m_A.cols());
+    assert(m_A.rows() == 3*m_mesh.getNodesNumber());
+    assert(m_b.rows() == 3*m_mesh.getNodesNumber());
 
     m_A.prune([this](int i, int j, float)
     {
