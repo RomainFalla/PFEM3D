2a9528e667b12a650a49defc3f6d07c6434577d0
ImperatorS79 <fevrier.simon@gmail.com>
2020-04-05 14:15:13 +0200

    Update Dragon2 scripts

--- run/DRAGON2/slurm.sh
+++ run/DRAGON2/slurm.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 # Submission script for Dragon2
 #SBATCH --job-name=PFEM test
-#SBATCH --time=05:00:00 # hh:mm:ss
+#SBATCH --time=5:00:00 # hh:mm:ss
 #
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=2625 # megabytes
 #SBATCH --partition=batch,long
 #
 #SBATCH --mail-user=simon.fevrier@student.uliege.be
-#SBATCH --mail-type=ALL
+#SBATCH --mail-type=END
 #
 #SBATCH --comment=PFEM
 #
 #SBATCH --outfile=PFEMTestOut.txt
 
-export OMP_NUM_THREADS=4
-export MKL_NUM_THREADS=4
+module load CMake/3.12.1-GCCcore-7.3.0
+module load CGAL/4.11.1-foss-2018b-Python-2.7.15
+
+export OMP_NUM_THREADS=32
+export MKL_NUM_THREADS=32
 
 cd $HOME/PFEM-master/build/bin
-srun ./pfem ../../params/params.json ../../geometry/square.msh results.msh
+srun ./pfem ../../params/3D/damBreakKoshizuka.json ../../geometry/3D/damBreakKoshizuka.msh results.msh
