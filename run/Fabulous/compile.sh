824e3198d456823f29e51255fa2d48918b42c3cd
ImperatorS79 <fevrier.simon@gmail.com>
2020-04-16 00:46:36 +0200

    Update build script for fabulous

--- run/Fabulous/compile.sh
+++ run/Fabulous/compile.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 #Do not forget to install libgfortran3  on your system!
 
-module load gcc/6.3.0 boost/1.78.0 gmp/6.0.2 mpfr/4.0.2 cmake/current CGAL/4.14.3 eigen/3.3.7
+module load gcc/9.3.0 boost/1.78.0 gmp/6.0.2 mpfr/4.0.2 cmake/current CGAL/4.14.3 eigen/3.3.7
 
 cd ../../
+
+if [ ! -d "lua" ]; then
+  wget https://www.lua.org/ftp/lua-5.3.5.tar.gz
+  tar -xf lua-5.3.5.tar.gz 
+  rm -rf lua-5.3.5.tar.gz
+  cd lua-5.3.5
+  sed -i 's?INSTALL_TOP= /usr/local?INSTALL_TOP= ../../lua?g' Makefile
+  sed -i 's?CFLAGS= -O2 -Wall -Wextra -DLUA_COMPAT_5_2 $(SYSCFLAGS) $(MYCFLAGS)?CFLAGS= -O2 -Wall -Wextra -fPIC -DLUA_COMPAT_5_2 $(SYSCFLAGS) $(MYCFLAGS)?g' src/Makefile
+  
+  make linux
+  make install
+  cd ../
+  rm -rf lua-5.3.5 
+fi
+
+if [ ! -d "sol3" ]; then
+  mkdir sol3
+  cd sol3
+  wget https://github.com/ThePhD/sol2/releases/download/v3.0.3/sol.hpp
+  wget https://github.com/ThePhD/sol2/releases/download/v3.0.3/forward.hpp
+  cd ../
+fi
 
 if [ ! -d "gmsh-4.5.4-Linux64-sdk" ]; then
   wget http://gmsh.info/bin/Linux/gmsh-4.5.4-Linux64-sdk.tgz
@@ -22,10 +44,13 @@ fi
 
 export GMSHSDK=${PWD}/gmsh-4.5.4-Linux64-sdk/
 export JSONSDK=${PWD}/nlohmann/
-export PATH=${GMSHSDK}/bin:${GMSHSDK}/lib:"${PATH}"
-export INCLUDE=${GMSHSDK}/include:"${INCLUDE}"
+export SOLSDK=${PWD}
+export LUASDK=${PWD}/lua/
+export PATH=${GMSHSDK}/bin:${GMSHSDK}/lib:${LUASDK}/bin:${LUASDK}/lib"${PATH}"
+export INCLUDE=${GMSHSDK}/include:${LUASDK}/include:"${INCLUDE}"
 export INCLUDE=${JSONSDK}:"${INCLUDE}"
-export LIB=${GMSHSDK}/lib:"${LIB}"
+export INCLUDE=${SOLSDK}:"${INCLUDE}"
+export LIB=${GMSHSDK}/lib:${LUASDK}/lib:"${LIB}"
 export PYTHONPATH=${GMSHSDK}/lib:"${PYTHONPATH}"
 export DYLD_LIBRARY_PATH=${GMSHSDK}/lib:"${DYLD_LIBRARY_PATH}"
 
--- run/Fabulous/slurm.sh
+++ run/Fabulous/slurm.sh
@@ -15,8 +15,10 @@
 #
 #SBATCH --output=PFEMTestOut.txt
 
+module load gcc/9.3.0 boost/1.78.0 gmp/6.0.2 mpfr/4.0.2 cmake/current CGAL/4.14.3
+
 export OMP_NUM_THREADS=12
 export MKL_NUM_THREADS=12
 
 cd $HOME/PFEM/build/bin
-srun ./pfem ../../params/3D/damBreakKoshizuka.json ../../geometry/3D/damBreakKoshizuka.msh results.msh
+srun ./pfem ../../params/3D/damBreakKoshizuka.json ../../geometry/3D/damBreakKoshizuka.msh
